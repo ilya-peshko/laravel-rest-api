cdcb7305b49ebbe5a6ac73a10615a66e7424a74c
Ilya Peshko <ilya.peshko@sibers.com>
2023-03-11 10:20:59 +0700

    9. added sanctum auth.

--- routes/api.php
+++ routes/api.php
@@ -20,9 +20,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'], function () {
+Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1', 'middleware' => 'auth:sanctum'], function () {
     Route::apiResource('customers', CustomerController::class);
     Route::apiResource('invoices', InvoiceController::class);
 
     Route::post('invoices/bulk', ['uses' => 'InvoiceController@bulkStore']);
 });
+
+//{"admin":"4|oukHJXh8QhZ1J2Rcq4BAzTlwDMQqcfSipKJI4Fst","update":"5|OXtAiY1iwNZ1kmYXLeJBbMBESQEbnOskw4WsH59N","basic":"6|IeEHlXofhPaTjKz5CQbshw7foC3CEnFXLNMoBbAd"}
