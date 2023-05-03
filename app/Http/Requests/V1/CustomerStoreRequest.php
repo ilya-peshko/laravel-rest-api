66022def9f9ee508ce42b3bbcc93455bd945aa75
Ilya Peshko <ilya.peshko@sibers.com>
2023-05-03 11:24:04 +0700

    13. renamed requests

--- app/Http/Requests/V1/CustomerStoreRequest.php
+++ app/Http/Requests/V1/CustomerStoreRequest.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests\V1;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
-class StoreCustomerRequest extends FormRequest
+class CustomerStoreRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
--- app/Http/Requests/V1/CustomerUpdateRequest.php
+++ app/Http/Requests/V1/CustomerUpdateRequest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 use Symfony\Component\HttpFoundation\Request;
 
-class UpdateCustomerRequest extends FormRequest
+class CustomerUpdateRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
